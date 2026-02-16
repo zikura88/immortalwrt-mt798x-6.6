378c1387fbd26829f0c0a32e08b20755c503de9b
zikura88 <982535189@qq.com>
2026-02-16 19:14:59 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -659,6 +659,20 @@ define Device/cmcc_rax3000m-emmc-mtk
 endef
 TARGET_DEVICES += cmcc_rax3000m-emmc-mtk
 
+define Device/cmcc_rax3000m-256m
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := RAX3000M 256M NAND
+  DEVICE_VARIANT := (H layout)
+  DEVICE_DTS := mt7981b-cmcc-rax3000m-256m
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 f2fsck mkf2fs
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 240128k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_rax3000m-256m
+
 define Device/cmcc_rax3000m-nand-mtk
   DEVICE_VENDOR := CMCC
   DEVICE_MODEL := RAX3000M NAND
